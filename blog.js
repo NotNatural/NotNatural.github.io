74ca6fb9fd074bd20cdb2e40aab98abfba629329
NotNatural <43958627+NotNatural@users.noreply.github.com>
2025-03-24 11:51:22 +0000

    Add sample javascript code for random rgb

---
+++ blog.js
@@ -0,0 +1,19 @@
+/*
+const image = document.querySelectorAll(".box");
+
+for(let i = 0; i < image.length; i++) {
+    image[i].addEventListener("mouseover");
+}
+
+
+function randomRGB() {
+    let RGBArray = [255, 255, 255];
+    let newRGB = RGBArray.map(x => x * Math.random());
+}
+
+let body = document.getElementByTagName("body")[0];
+body.addEventListener("click", function() {
+body.style.background = randomRGB();
+});
+
+*/
